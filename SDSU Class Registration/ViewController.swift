9ade4810953b26912a4046e65ef6bb096fb576be
Vyshak Athreya Bellur Keshavamurthy <vyshakathreya@gmail.com>
2018-04-06 01:38:18 -0700

    Created Login Screen

--- SDSU Class Registration/ViewController.swift
+++ SDSU Class Registration/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var emailIdLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var redIdLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var emailIdTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var redIdTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func signUpSwitchChanged(_ sender: UISwitch) {
+        if(sender.isOn){
+            firstNameLabel.isHidden = false;
+            emailIdLabel.isHidden = false;
+            lastNameLabel.isHidden = false;
+            emailIdTextField.isHidden = false;
+            firstNameTextField.isHidden = false;
+            lastNameLabel.isHidden = false;
+        }else{
+            firstNameLabel.isHidden = true;
+            emailIdLabel.isHidden = true;
+            lastNameLabel.isHidden = true;
+            emailIdTextField.isHidden = true;
+            firstNameTextField.isHidden = true;
+            lastNameLabel.isHidden = true;
+        }
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hideKeyboard()
+    }
+    
+    func hideKeyboard(){
+        view.endEditing(true);
+    }
+    
+    
 }
 
