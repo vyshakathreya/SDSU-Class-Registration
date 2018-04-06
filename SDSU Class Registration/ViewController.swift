12847a254022698c24de7149bd46a5af11265fc3
Vyshak Athreya Bellur Keshavamurthy <vyshakathreya@gmail.com>
2018-04-06 00:56:46 -0700

    Initial Commit

---
+++ SDSU Class Registration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDSU Class Registration
+//
+//  Created by Vyshak Athreya B K on 4/6/18.
+//  Copyright © 2018 Vyshak Athreya B K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SDSU Class RegistrationTests/SDSU_Class_RegistrationTests.swift
@@ -0,0 +1,36 @@
+//
+//  SDSU_Class_RegistrationTests.swift
+//  SDSU Class RegistrationTests
+//
+//  Created by Vyshak Athreya B K on 4/6/18.
+//  Copyright © 2018 Vyshak Athreya B K. All rights reserved.
+//
+
+import XCTest
+@testable import SDSU_Class_Registration
+
+class SDSU_Class_RegistrationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
